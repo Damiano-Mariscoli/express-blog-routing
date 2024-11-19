dfaa9385667636329a270e1f8abbb0d2c2619246
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-11-19 16:33:40 +0100

    ADDED: data folder

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 const express = require("express");
-const postsRouter = require("./routers/posts.js")
+const postsRouter = require("./routers/posts.js");
 const app = express();
 const port = 3000;
-const postsList = [
-  { title: "", content: "", img: "./public/images/ciambellone.jpeg", tags: ["", "", ""] },
-  { title: "", content: "", img: "./public/images/cracker_barbabietola.jpeg", tags: ["", "", ""] },
-  { title: "", content: "", img: "./public/images/pane_fritto_dolce.jpeg", tags: ["", "", ""] },
-  { title: "", content: "", img: "./public/images/pasta_barbabietola.jpeg", tags: ["", "", ""] },
-  { title: "", content: "", img: "./public/images/torta_paesana.jpeg", tags: ["", "", ""] },
-];
 
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.use('/posts' , postsRouter)
-
+app.use("/posts", postsRouter);
 
 app.get("/bacheca", (req, res) => {
-    const postCount = posts.length;
-    res.json({ posts: posts, count: postCount });
+  const postCount = posts.length;
+  res.json({ posts: posts, count: postCount });
 });
 
 app.listen(port, () => {
   console.log("server");
 });
-
-
-module.exports = postsList;
--- routers/posts.js
+++ routers/posts.js
@@ -1,45 +1,38 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const postsList = require("../data/posts");
 
-const router = express.Router()
-const postsList = [
-    { title: "0", content: "", img: "./public/images/ciambellone.jpeg", tags: ["", "", ""] },
-    { title: "1", content: "", img: "./public/images/cracker_barbabietola.jpeg", tags: ["", "", ""] },
-    { title: "2", content: "", img: "./public/images/pane_fritto_dolce.jpeg", tags: ["", "", ""] },
-    { title: "3", content: "", img: "./public/images/pasta_barbabietola.jpeg", tags: ["", "", ""] },
-    { title: "4", content: "", img: "./public/images/torta_paesana.jpeg", tags: ["", "", ""] },
-  ];
+
+
 
 //index
-router.get('/', (req, res) => {
-    res.json(postsList);
-})
-
+router.get("/", (req, res) => {
+  res.json(postsList);
+});
 
 //show
-router.get('/:id', (req, res) =>{
-    res.json(postsList[req.params.id])
-})
-
+router.get("/:id", (req, res) => {
+  res.json(postsList[req.params.id]);
+});
 
 //store
-router.post('/' , (req , res) =>{
-    res.send('Creo un nuovo post')
-})
+router.post("/", (req, res) => {
+  res.send("Creo un nuovo post");
+});
 
 //update
-router.put('/:id' , (req, res) =>{
-    res.send(`Modifico il post con id: ${req.params.id}`)
-})
+router.put("/:id", (req, res) => {
+  res.send(`Modifico il post con id: ${req.params.id}`);
+});
 
 //modify
-router.patch('/:id' , (req , res) =>{
-    res.send(`Aggiorno il post con id: ${req.params.id}`)
-})
+router.patch("/:id", (req, res) => {
+  res.send(`Aggiorno il post con id: ${req.params.id}`);
+});
 
 //destroy
-router.delete('/:id' , (req, res) =>{
-    res.send(`Elimino il post con id ${req.params.id}`)
-})
+router.delete("/:id", (req, res) => {
+  res.send(`Elimino il post con id ${req.params.id}`);
+});
 
-
-module.exports = router
+module.exports = router;
